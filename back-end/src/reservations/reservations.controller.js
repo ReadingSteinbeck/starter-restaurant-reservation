5a50cedb1c4c2637462df65909ec399c62c82859
Alex Abedi <alexanderabedi@gmail.com>
2022-01-13 16:36:28 -0500

    more debugging isInFuture  backend

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -106,9 +106,10 @@ function reservationDateAndTimeAreValid(req, res, next) {
 
 function isInFuture(req, res, next) {
   const { reservation_time, reservation_date } = res.locals;
-  console.log(`Time: ${reservation_time}, Date: ${reservation_date}`);
   const reservationDateAndTime = parseInt(reservation_date + reservation_time);
   const now = getDateAndTimeHelper();
+  console.log(`Time: ${reservation_time}, Date: ${reservation_date}`);
+  console.log(`resDateTime ${reservationDateAndTime}, now ${now}`);
 
   if (now < reservationDateAndTime) {
     return next();
