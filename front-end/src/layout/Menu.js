45686d73cb8e508b21d42f472be092e1246f5e61
Alex Abedi <alexanderabedi@gmail.com>
2022-01-18 17:06:03 -0500

    responsive menu

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import useMediaQuery from "../utils/useMediaQuery";
 
 /**
  * Defines the menu for this application.
@@ -9,6 +10,101 @@ import { Link } from "react-router-dom";
  */
 
 function Menu() {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const isMobile = useMediaQuery("(max-width: 503px)");
+
+  function LinksList({ isDesktop }) {
+    if (isDesktop)
+      return (
+        <div className="nav navbar-nav text-light base" id="accordionSidebar">
+          <div className="nav-item">
+            <Link className="nav-link " to="/dashboard">
+              <span className="oi oi-dashboard" />
+              &nbsp;Dashboard
+            </Link>
+          </div>
+          <div className="nav-item">
+            <Link className="nav-link " to="/search">
+              <span className="oi oi-magnifying-glass" />
+              &nbsp;Search
+            </Link>
+          </div>
+          <div className="nav-item">
+            <Link className="nav-link " to="/reservations/new">
+              <span className="oi oi-plus" />
+              &nbsp;New Reservation
+            </Link>
+          </div>
+          <div className="nav-item">
+            <Link className="nav-link " to="/tables/new">
+              <span className="oi oi-layers" />
+              &nbsp;New Table
+            </Link>
+          </div>
+        </div>
+      );
+    if (isMobile)
+      return (
+        <div
+          className="nav navbar-nav text-light d-flex justify-content-around flex-row "
+          id="accordionSidebar"
+        >
+          <div className="nav-item  p-2">
+            <Link className="nav-link " to="/dashboard">
+              <span className="oi oi-dashboard" />
+            </Link>
+          </div>
+          <div className="nav-item p-2">
+            <Link className="nav-link " to="/search">
+              <span className="oi oi-magnifying-glass" />
+            </Link>
+          </div>
+          <div className="nav-item p-2">
+            <Link className="nav-link " to="/reservations/new">
+              <span className="oi oi-plus" />
+            </Link>
+          </div>
+          <div className="nav-item p-2">
+            <Link className="nav-link " to="/tables/new">
+              <span className="oi oi-layers" />
+            </Link>
+          </div>
+        </div>
+      );
+
+    return (
+      <div
+        className="nav navbar-nav text-light d-flex justify-content-around flex-row "
+        id="accordionSidebar"
+      >
+        <div className="nav-item  p-2">
+          <Link className="nav-link " to="/dashboard">
+            <span className="oi oi-dashboard" />
+            &nbsp;Dashboard
+          </Link>
+        </div>
+        <div className="nav-item p-2">
+          <Link className="nav-link " to="/search">
+            <span className="oi oi-magnifying-glass" />
+            &nbsp;Search
+          </Link>
+        </div>
+        <div className="nav-item p-2">
+          <Link className="nav-link " to="/reservations/new">
+            <span className="oi oi-plus" />
+            &nbsp;New Reservation
+          </Link>
+        </div>
+        <div className="nav-item p-2">
+          <Link className="nav-link " to="/tables/new">
+            <span className="oi oi-layers" />
+            &nbsp;New Table
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <nav className="navbar navbar-dark align-items-start p-0">
       <div className="container-fluid d-flex flex-column p-0">
@@ -21,32 +117,8 @@ function Menu() {
           </div>
         </Link>
         <hr className="sidebar-divider my-0" />
-        <ul className="nav navbar-nav text-light" id="accordionSidebar">
-          <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">
-              <span className="oi oi-dashboard" />
-              &nbsp;Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/search">
-              <span className="oi oi-magnifying-glass" />
-              &nbsp;Search
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/reservations/new">
-              <span className="oi oi-plus" />
-              &nbsp;New Reservation
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/tables/new">
-              <span className="oi oi-layers" />
-              &nbsp;New Table
-            </Link>
-          </li>
-        </ul>
+
+        <LinksList isDesktop={isDesktop} />
         <div className="text-center d-none d-md-inline">
           <button
             className="btn rounded-circle border-0"
