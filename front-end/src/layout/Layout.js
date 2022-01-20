09d8faa09ea77ec996c78b629d4df1a50e38baab
Alex Abedi <alexanderabedi@gmail.com>
2022-01-19 20:07:02 -0500

    cleaned up Menu.js

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -2,8 +2,6 @@ import React from "react";
 import Menu from "./Menu";
 import Routes from "./Routes";
 
-import useMediaQuery from "../utils/useMediaQuery";
-
 import "./Layout.css";
 
 /**
@@ -14,27 +12,12 @@ import "./Layout.css";
  * @returns {JSX.Element}
  */
 function Layout() {
-  const isDesktop = useMediaQuery("(min-width: 768px)");
-  if (isDesktop)
-    return (
-      <div className="container-fluid">
-        <div className="row h-100">
-          <div className="col-12 side-bar">
-            <Menu />
-          </div>
-          <div className="col">
-            <Routes />
-          </div>
-        </div>
-      </div>
-    );
   return (
     <div className="container-fluid">
       <div className="row h-100">
         <div className="col-12 side-bar">
           <Menu />
         </div>
-
         <div className="col">
           <Routes />
         </div>
